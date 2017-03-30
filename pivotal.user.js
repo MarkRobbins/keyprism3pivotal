a385df2dea8873bfb943e151f089bec0ff2f6361
Mark Robbins <mark.robbins@mrobbinsassoc.com>
2017-03-30 13:06:06 -0700

    Empty UserScript

---
+++ pivotal.user.js
@@ -0,0 +1,20 @@
+// has embedded jQuery 2.1.4
+// ==UserScript==
+// @name         Keyprism3 Pivotal
+// @namespace    http://mrobbinsassoc.com/
+// @version      0.0.1
+// @description  Allows interaction of Pivotal Tracker, Yodiz, TheBrain and Visual Paradigm
+// @author       Mark Robbins
+// @match        https://www.pivotaltracker.com/n/projects/1997711*
+// @grant        none
+// @copyright    2017, Mark Robbins
+// @homepageURL  http://mrobbinsassoc.com/projects/keyprism3/vp/publish/
+// @supportURL   mailto:mark.robbins@mrobbinsassoc.com
+// @updateURL    https://openuserjs.org/meta/MarkRobbins/Keyprism3_Pivotal.meta.js
+// ==/UserScript==
+// ==OpenUserJS==
+// @author MarkRobbins
+// ==/OpenUserJS==
+
+
+console.log('Keyprism3 Pivotal');
